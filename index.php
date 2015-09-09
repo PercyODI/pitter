8079359c5bce11d3759983580a39b042e41dde6a
PercyODI <pearse.2008@gmail.com>
2015-09-08 20:47:32 -0400

    Tweaks to index.php

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
 <html lang="en">
     <head>
         <title>Pearse's Digital Ocean Test Page</title>
-        <!-- <link rel="stylesheet" type="text/css" href="styling.css"> -->
+         <link rel="stylesheet" type="text/css" href="styling.css"> 
         <link href="css/bootstrap.css" rel="stylesheet">
     </head>
     <body>
-        <div class="container">
-            <h1 class="jumbotron">Welcome to Pitter!</h1>
+        <div class="container" style="background-color: lightgrey">
+            <h1 class="jumbotron centerfy">Welcome to Pitter!</h1>
             <?php
                 if(isset($_SESSION['message'])) {
                     echo "<div class='alert alert-info fade in'><a href='#' class='close' data-dismiss='alert'>&times;</a>" . $_SESSION['message'] . "</div>";
@@ -22,40 +22,39 @@
                 }
             ?>
             <form method='post' action='add.php'>
-            <p>Your status:</p>
-            <textarea name='body' rows='5' cols='40' wrap=VIRTUAL></textarea>
-            <p><input type='submit' value='submit'></p>
+                <p>Your status:</p>
+                <textarea class="form-control" rows="2"></textarea>
+                <p><input type='submit' value='submit'></p>
             </form>
+            <?php
+                $users = show_users($_SESSION['userid']);
+                if(count($users)) {
+                    $myusers = array_keys($users);
+                }
+                else {
+                    $myusers = array();
+                }
+                $myusers[] = $_SESSION['userid'];
 
-            <div class='row'>
-                <?php
-                    $users = show_users($_SESSION['userid']);
-                    if(count($users)) {
-                        $myusers = array_keys($users);
-                    }
-                    else {
-                        $myusers = array();
-                    }
-                    $myusers[] = $_SESSION['userid'];
+                $posts = show_posts($myusers, 10);
 
-                    $posts = show_posts($myusers, 10);
-
-                    if (count($posts)){
-                        echo "<div class='postList'>";
-                        foreach ($posts as $key => $list){
-                            echo "<div class='col-md-4' style='background-color: #E40606; border: 2px solid #B10000; padding: 15px;'>";
-                            echo "<div>" . find_username($list['userid']) . "</div><br>";
-                            echo "<div>" . $list['body'] . "</div>";
-                            echo "<div>".$list['stamp'] . "</div>";
-                            echo "</div>";
-                        }
-                        echo "</div>";
-                    } 
-                    else {
-                        echo "<p><b>You haven't posted anything yet!</b></p>";
+                if (count($posts)){
+                    echo "<div class='row'>\n";
+                    foreach ($posts as $key => $list){ 
+                        echo "<div class='col-md-4'>\n";
+                            echo "<div style='background-color: #E40606; border: 2px solid #B10000;'>\n";
+                                echo "<div>" . find_username($list['userid']) . "</div><br>\n";
+                                echo "<div>" . $list['body'] . "</div>\n";
+                                echo "<div>".$list['stamp'] . "</div>\n";
+                            echo "</div>\n";
+                        echo "</div>\n";
                     }
-                ?>
-            </div>
+                    echo "</div>\n";
+                } 
+                else {
+                    echo "<p><b>You haven't posted anything yet!</b></p>\n";
+                }
+            ?>
             <p><a href='users.php'>see list of users</a></p>
             <h2>Users you're following</h2>
 
